aaab3554ffdd142ea39105ea3aaec2928f60bd66
dylee00 <dylee@sookmyung.ac.kr>
2024-01-27 17:01:09 +0900

    access token시간 변경
    access token시간 변경

--- prolog-backend/src/main/java/prologbackend/jwt/TokenProvider.java
+++ prolog-backend/src/main/java/prologbackend/jwt/TokenProvider.java
@@ -27,7 +27,7 @@ public class TokenProvider {
 //    private static final Logger log = LoggerFactory.getLogger(TokenProvider.class);
     private static final String AUTHORITIES_KEY = "auth";
     private static final String BEARER_TYPE = "bearer";
-    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30;
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 300;
     private final Key key;
 
     public TokenProvider(@Value("${jwt.secret}") String secretKey) {
