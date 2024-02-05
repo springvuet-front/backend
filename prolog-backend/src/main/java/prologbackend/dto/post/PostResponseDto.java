4e2cde69466320ec5b90efbec13888cf2f3d6684
dylee00 <dylee@sookmyung.ac.kr>
2024-02-05 13:57:03 +0900

    commit
    commit

--- prolog-backend/src/main/java/prologbackend/dto/post/PostResponseDto.java
+++ prolog-backend/src/main/java/prologbackend/dto/post/PostResponseDto.java
@@ -3,6 +3,8 @@ package prologbackend.dto.post;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
 
 @Getter
 public class PostResponseDto {
@@ -12,13 +14,23 @@ public class PostResponseDto {
     private String postTitle;
     private String postContent;
     private LocalDateTime createAt;
+    private UUID postUuid;
 
-    public PostResponseDto(String nickname, String postCategory, String postStatus, String postTitle, String postContent, LocalDateTime createAt) {
+    public PostResponseDto(String nickname, String postCategory, String postStatus, String postTitle, String postContent, LocalDateTime createAt, UUID postUuid) {
         this.nickname = nickname;
         this.postCategory = postCategory;
         this.postStatus = postStatus;
         this.postTitle = postTitle;
         this.postContent = postContent;
         this.createAt = createAt;
+        this.postUuid = postUuid;
+    }
+
+    public String getCreate() {
+        if (createAt == null) {
+            return null;
+        }
+        return createAt.format(DateTimeFormatter.ISO_DATE_TIME);
+
     }
 }
